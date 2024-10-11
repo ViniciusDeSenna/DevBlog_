3adfc4d60851b3e4d68dea2d9e3969b383c725df
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-10-11 14:41:18 -0300

    Pagina de perfil

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="DevBlog_" width="30" height="24">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Trend Topics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Communities</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <div class="dropdown">
+                    <button class="btn btn-link dropdown-toggle" type="button" id="userMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="https://avatars.githubusercontent.com/u/110927988?v=4" alt="Avatar" class="rounded-circle" width="32" height="32">
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="userMenuButton">
+                        <a class="dropdown-item" href="#">Perfil</a>
+                        <a class="dropdown-item" href="#">Configurações</a>
+                        <a class="dropdown-item" href="#">Sair</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,6 +14,16 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    </head>
     <body>
         @include('components.navbar')
+        <div class="container position-relative">
+            <img src="https://avatars.githubusercontent.com/u/110927988?v=4" class="img-fluid w-100 mt-3" alt="Banner" style="object-fit: cover; height: 300px;">
+            <img src="https://avatars.githubusercontent.com/u/110927988?v=4" class="img-thumbnail position-absolute" alt="Perfil" style="top: 150px; left: 20px; width: 200px; height: auto;">
+        </div>
+        <div class="container mt-5">
+            <h3>
+                Vinícius de Senna
+                <small class="text-body-secondary">The Creator</small>
+            </h3>
+        </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
